9e6b085e99fbc11d6b57b5185e0e53fbb90829f7
btxmkbtx <btxmkbtx@gmail.com>
2025-10-22 12:34:53 +0900

    解决中间件重定向login画面时，url不刷新的问题

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,17 @@
+'use client'
 import { LoginForm } from '@/components/auth/login-form'
+import { usePathname, useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function Login() {
+  const router = useRouter()
+  const currentPathname = usePathname()
+
+  useEffect(() => {
+    // 因为middleware.ts的重定向导致页面内容虽然重新描绘，但是url不正确，所以这里再做一次校验
+    if (currentPathname !== '/auth/login') {
+      router.replace('/auth/login')
+    }
+  }, [router, currentPathname])
   return <LoginForm />
 }
